ed409b2b2586aeab2c76380ac8d70250f566fd5d
michael <michael@mini.fritz.box>
2017-01-19 07:58:35 +0100

    Caesar chipher works now.

--- encryption/SymmetricEncryption.java
+++ encryption/SymmetricEncryption.java
@@ -14,49 +14,66 @@ import java.util.regex.Pattern;
  */
 public class SymmetricEncryption
 {
-  public static boolean isStringValid(String input)
-  {
-      Pattern p = Pattern.compile("[^A-Z]+");
-      Matcher m = p.matcher(input);
-      
-      if (m.find()) 
-      {
-          return false;
-      } 
-      else 
-      {
-          return true;
-      }
-  }
-  
-  public static String caesar(String input, int shift)
-  {
-      if (isStringValid(input) == false) {
-          throw new IllegalArgumentException("Invalid input string.");
-      }
-      
-      char[] inputChars = input.toCharArray();
-      char[] crypted = new char[inputChars.length];
-      
-      for (int i = 0; i < inputChars.length; i++) {
-          int posInAlphabet = (inputChars[i] - 65 + shift) % 26;
-          
-          crypted[i] = (char)((posInAlphabet + 65));
-      }
-      
-      return String.copyValueOf(crypted);
-  }
-  public static String rot13(String input) { 
-      return caesar(input, 13);
-  }
-  
-  public static String vigenereEncrypt(String input, String key)
-  {
-      return "";
-  }
-  
-  public static String vigenereDecrypt(String input, String key) {
-      return "";
-  }
+    public static boolean isStringValid(String input)
+    {
+        Pattern p = Pattern.compile("[^A-Z]+");
+        Matcher m = p.matcher(input);
 
+        if (m.find()) 
+        {
+            return false;
+        } 
+      
+        return true;
+    }
+
+    public static String caesar(String input, int shift)
+    {
+        if (!isStringValid(input)) 
+        {
+            throw new IllegalArgumentException("Invalid input string.");
+        }
+
+        char[] inputChars = input.toCharArray();
+        char[] crypted = new char[inputChars.length];
+
+        for (int i = 0; i < inputChars.length; i++) 
+        {
+            int posInAlphabet = (inputChars[i] - 65 + shift) % 26;
+
+            while (posInAlphabet < 0) 
+            {
+                posInAlphabet += 26;
+            }
+
+            crypted[i] = (char)((posInAlphabet + 65));
+        }
+
+        return String.copyValueOf(crypted);
+    }
+    
+    public static String rot13(String input) 
+    { 
+        return caesar(input, 13);
+    }
+    
+    public static String createValidKey(String keyBase, int exclusiveLimit) {
+        String validKey = keyBase;
+        
+        while (validKey.length() < exclusiveLimit) {
+            validKey += keyBase;
+        }
+        
+        return validKey.substring(0, exclusiveLimit);
+    }
+
+    public static String vigenereEncrypt(String input, String key)
+    {
+        return "";
+    }
+
+    public static String vigenereDecrypt(String input, String key) 
+    {
+        return "";
+    }
 }
--- encryption/TestSymetricEncryption.java
+++ encryption/TestSymetricEncryption.java
@@ -12,16 +12,19 @@ import static java.lang.System.*;
  */
 public class TestSymetricEncryption
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         out.println(SymmetricEncryption.isStringValid("ABCDEF"));
         out.println(SymmetricEncryption.isStringValid("ABC1DEF"));
         out.println(SymmetricEncryption.isStringValid("ABC DEF"));
         out.println(SymmetricEncryption.caesar("ABCXYZ", 1));
-        out.println(SymmetricEncryption.caesar("ABCXYZ", 3));
+        out.println(SymmetricEncryption.caesar("HALLO", -3));
         
         String tmp = SymmetricEncryption.rot13("ABCXYZ");
         
         out.println(tmp);
         out.println(SymmetricEncryption.rot13(tmp));
+        
+        out.println(SymmetricEncryption.createValidKey("abc", "ONETWOTHREE".length()));
     }
 }
